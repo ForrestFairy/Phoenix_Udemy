838f308a80454a79d63da90160d5788a80596ac3
ForrestFairy <szymon.bogusz55@gmail.com>
2021-05-24 11:58:46 +0200

    Users can now add comments to specific topics It does not appear immediately on the screen yet

---
+++ discuss/lib/discuss_web/channels/comments_channel.ex
@@ -0,0 +1,29 @@
+defmodule DiscussWeb.CommentsChannel do
+  use DiscussWeb, :channel
+
+  alias DiscussWeb.{Topic, Comment}
+
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Topic
+      |> Discuss.Repo.get(topic_id)
+      |> Discuss.Repo.preload(:comments)
+
+    {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
+  end
+
+  def handle_in(name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+
+    changeset = topic
+    |> Ecto.build_assoc(:comments)
+    |> Comment.changeset(%{content: content})
+
+    case Discuss.Repo.insert(changeset) do
+      {:ok, comment} ->
+        {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
+  end
+end
