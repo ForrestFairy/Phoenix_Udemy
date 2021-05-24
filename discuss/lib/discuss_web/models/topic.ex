5ca11b95fad3f21f5da830d9fd44489d3f071fa7
ForrestFairy <szymon.bogusz55@gmail.com>
2021-05-24 09:38:52 +0200

    Created comment table to store coments for all topics

--- discuss/lib/discuss_web/models/topic.ex
+++ discuss/lib/discuss_web/models/topic.ex
@@ -4,6 +4,7 @@ defmodule DiscussWeb.Topic do
   schema "topics" do
     field :title, :string
     belongs_to :user, DiscussWeb.Users
+    has_many :comments, DiscussWeb.Comment
   end
 
   def changeset(struct, params \\ %{}) do
